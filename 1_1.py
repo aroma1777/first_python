bd4aa3337ec1ee16684e10ecb2a71afb9048631e
aroma1777 <aromaelias77@gmail.com>
2022-07-18 18:22:19 +0300

    Simple calc

---
+++ 1_1.py
@@ -0,0 +1,15 @@
+###
+### First question
+### Create a simple calculator program that accepts 2 input and then displays the sum,subtraction, multiple and qotient of the two input
+
+f1=input("Write the first number: ")
+f1=int(f1)
+f2=input("Write the second number: ")
+f2=int(f2)
+
+print("The sum of the two numbers is ",f1+f2)
+print("The subtract of the two numbers is ",f1-f2)
+print("The multiple of the two numbers is ",f1*f2)
+print("The qoutient of the two number is ",f1/f2)
+
+print("The reminder of first number divided by second number is ",f1%f2)
